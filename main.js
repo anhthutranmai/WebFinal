09d440a26d77b5656b46b7762f842fd871de2383
JaeySen <longhientranjjj@gmail.com>
2021-03-15 20:02:45 +0700

    form update
    input option count and log

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let more = document.querySelector(".btn.btn-outline-primary");
-
+var i = 1;
 more.addEventListener("click",function(e) {
   e.preventDefault();
 
   let newinput = document.createElement("form-group");
-
-  newinput.innerHTML = '<label for="formGroupExampleInput">Option </label><input type="text" class="form-control" id="formGroupExampleInput" placeholder="Another input">';
-
+  i += 1;
+  newinput.innerHTML = "<label for='formGroupExampleInput" + i + "'>Option "+ i +"</label><input type='text' class='form-control' id='formGroupExampleInput" + i +"' placeholder='Another input'>";
+  console.log("now u have "+ i +" option");
   let where = document.querySelector("form");
   where.append(newinput);
 
