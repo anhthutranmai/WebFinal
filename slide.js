17c384a5822154d78ec3dac1824aa323b665b26a
JaeySen <longhientranjjj@gmail.com>
2021-03-23 20:21:43 +0700

    commit

--- slide.js
+++ slide.js
@@ -7,36 +7,19 @@ document.addEventListener("DOMContentLoaded",function(){
   console.log("laod");
   console.log(storedArr);
   for (var i = 0; i < storedArr.length; i++) {
-    // let quiz = `
-    // <div class="row shadow-sm ml-50 mr-50 p-10 position-relative">
-    //   <a class="btn btn-outline-danger"><i class="fas fa-level-up-alt"></i></a>
-    //   <a class="btn btn-outline-primary"><i class="fas fa-level-down-alt"></i></a>
-    //   <div class="justify-content-end">
-    //     ${storedArr[i]}
-    //   </div>
-    // </div>`;
-    // console.log(storedArr[i]);
-    // slide.innerHTML = quiz;
-
     let quiz = document.createElement("div");
     quiz.classList.add("row");
     quiz.classList.add("shadow-sm");
     quiz.classList.add("ml-50");
     quiz.classList.add("mr-50");
-    quiz.classList.add("p-10");
+    quiz.classList.add("py-2");
+    quiz.classList.add("px-1");
     quiz.classList.add("position-relative");
     slide.appendChild(quiz);
     // g.setAttribute("id", "Div1"); to add id
-    let up = document.createElement("a");
-    up.classList.add("btn");
-    up.classList.add("btn-outine-danger");
-    let upicon = document.createElement("i");
-    upicon.classList.add("fas");
-    upicon.classList.add("fa-level-up-alt");
-    up.appendChild(upicon);
-    quiz.appendChild(up);
 
     let dw = document.createElement("a");
+    dw.setAttribute("role","button");
     dw.classList.add("btn");
     dw.classList.add("btn-outline-primary");
     let dwicon = document.createElement("i");
@@ -45,22 +28,102 @@ document.addEventListener("DOMContentLoaded",function(){
     dw.appendChild(dwicon);
     quiz.appendChild(dw);
 
-    let q = document.createElement("div");
-    q.classList.add("justify-content-end");
+    let up = document.createElement("a");
+    up.setAttribute("role","button");
+    up.classList.add("btn");
+    up.classList.add("btn-outine-warning");
+    let upicon = document.createElement("i");
+    upicon.classList.add("fas");
+    upicon.classList.add("fa-level-up-alt");
+    up.appendChild(upicon);
+    quiz.appendChild(up);
+
+    let q = document.createElement("p");
+    // q.classList.add("justify-content-end");
+    q.classList.add("text-center");
     let content = document.createTextNode(`${storedArr[i]}`);
     q.appendChild(content);
     quiz.appendChild(q);
   }
-  })
-    // storedArr.forEach(function(i){
-    //     let quiz = `
-    //     <div class="row shadow-sm ml-50 mr-50 p-10 position-relative">
-    //       <a class="btn btn-outline-danger"><i class="fas fa-level-up-alt"></i></a>
-    //       <a class="btn btn-outline-primary"><i class="fas fa-level-down-alt"></i></a>
-    //       <div class="justify-content-end">
-    //         ${storedArr[i]}
-    //       </div>
-    //     </div>`;
-    //     console.log(storedArr[i]);
-    //     slide.innerHTML = quiz;
-    // });
+
+  //fetch comment from jsonplaceholder
+  getUser();
+  getComment();
+})
+let cmt = document.querySelector("#comment");
+
+function getUser(){
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET","https://randomuser.me/api",true);
+  xhr.onload = function() {
+    if(this.status == 200) {
+        let res = JSON.parse(this.responseText);
+        console.log(res.results[0].picture.thumbnail);
+        let ava = document.createElement("img");
+        ava.classList.add("round-circle");
+        ava.setAttribute("src",`${res.results[0].picture.thumbnail}`);
+        cmt.appendChild(ava);
+
+        let name = document.createElement("h5");
+        name.classList.add("font-weight-bold");
+        let n = document.createTextNode(`${res.results[0].name.first} ${res.results[0].name.last}`);
+        name.appendChild(n);
+        cmt.appendChild(name);
+
+        // for (var i = 0; i < 3; i++) {
+        //   console.log(res[i]);
+        //   let newcmt = document.createElement("div");
+        //   let user = document.createElement("a");
+        //   let username = document.createTextNode(`${res[i].name}`);
+        //   user.appendChild(username);
+        //   cmt.appendChild(user);
+        //
+        //   let line = document.createElement("p");
+        //   let text = document.createTextNode(`${res[i].body}`);
+        //   line.appendChild(text);
+        //   cmt.appendChild(line);
+        // }
+      
+      }
+  }
+  xhr.send();
+
+
+}
+
+function getComment(){
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET","https://jsonplaceholder.typicode.com/comments",true);
+  xhr.onload = function() {
+    let res = JSON.parse(this.responseText);
+    console.log(res[0]);
+    let line = document.createElement("p");
+    let text = document.createTextNode(`${res[0].body}`);
+    line.appendChild(text);
+    cmt.appendChild(line);
+  }
+
+  xhr.send();
+}
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("GET","http://jsonplaceholder.typicode.com/comments",true);
+  // xhr.onload = function() {
+  //   if(this.status == 200) {
+  //       let res = JSON.parse(this.responseText);
+  //       for (var i = 0; i < 3; i++) {
+  //         console.log(res[i]);
+  //         let newcmt = document.createElement("div");
+  //         newcmt.classList.add("row");
+  //         let user = document.createElement("a");
+  //         let username = document.createTextNode(`${res[i].name}`);
+  //         user.appendChild(username);
+  //         cmt.appendChild(user);
+  //
+  //         let line = document.createElement("p");
+  //         let text = document.createTextNode(`${res[i].body}`);
+  //         line.appendChild(text);
+  //         cmt.appendChild(line);
+  //       }
+  //     }
+  // }
+  // xhr.send();
